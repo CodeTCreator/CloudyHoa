cf57e0f194604aabd3816261b00ab8fa61117243
Богдан Т <bogdantribusyan2016@gmail.com>
2023-12-26 21:57:35 +0300

    Создание сервиса "ObjectsService".

---
+++ WcfServiceLibrary/IObjectsService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IObjectsService" in both code and config file together.
+    [ServiceContract]
+    public interface IObjectsService
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
