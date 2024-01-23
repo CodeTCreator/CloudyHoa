0a7526c7557495da8d4b0630399919ef90abb5e8
Богдан Т <bogdantribusyan2016@gmail.com>
2024-01-23 13:54:39 +0300

    Добавления сервиса для работы с лицевым счетом.

---
+++ WcfServiceLibrary/PersonalAccountService/IPersonalAccountService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IPersonalAccountService" in both code and config file together.
+    [ServiceContract]
+    public interface IPersonalAccountService
+    {
+        [OperationContract]
+        void addPersonalAccount(string account, int object_id);
+
+        void deletePersonalAccount(int id);
+
+        void editPersonalAccount(int id, string number, int object_id);
+
+    }
+}
---
+++ WcfServiceLibrary/PersonalAccountService/PersonalAccountService.cs
@@ -0,0 +1,62 @@
+﻿using Devart.Data.PostgreSql;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Xml.Linq;
+
+namespace WcfServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "PersonalAccountService" in both code and config file together.
+    public class PersonalAccountService : IPersonalAccountService
+    {
+
+        static IConfiguration configurationDB = new ConfigurationBuilder().AddJsonFile("AppSettings.json").Build();
+
+        readonly string connectionString = configurationDB["AppSettings:DatabaseConnection"];
+
+        public void addPersonalAccount(string account, int object_id)
+        {
+            using (PgSqlConnection conn = new PgSqlConnection(connectionString))
+            {
+                conn.Open();
+                PgSqlCommand pgSqlCommand = new PgSqlCommand("INSERT INTO personal_account (account, object_id) VALUES (@account,@object_id)"
+                    , conn);
+                pgSqlCommand.Parameters.Add("@account", account);
+                pgSqlCommand.Parameters.Add("@object_id", object_id);
+                pgSqlCommand.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+
+        public void deletePersonalAccount(int id)
+        {
+            using (PgSqlConnection conn = new PgSqlConnection(connectionString))
+            {
+                conn.Open();
+                PgSqlCommand pgSqlCommand = new PgSqlCommand("Delete from personal_account where id = @id_value", conn);
+                pgSqlCommand.Parameters.Add("@id_value", id);
+                pgSqlCommand.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+
+        public void editPersonalAccount(int id, string account, int object_id)
+        {
+            using (PgSqlConnection conn = new PgSqlConnection(connectionString))
+            {
+                conn.Open();
+                PgSqlCommand pgSqlCommand = new PgSqlCommand("UPDATE personal_account SET account = @account, object_id = @object_id" +
+                    " where id = @id", conn);
+                pgSqlCommand.Parameters.Add("@id", id);
+                pgSqlCommand.Parameters.Add("@account", account);
+                pgSqlCommand.Parameters.Add("@object_id", object_id);
+                pgSqlCommand.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+    }
+}
