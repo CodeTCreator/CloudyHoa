b713bba46beceb7170f0573151ff8340122087fe
Богдан Т <bogdantribusyan2016@gmail.com>
2024-03-02 23:14:28 +0300

    Создан сервис метаданных.

---
+++ WcfServiceLibrary/MetadataService/MetadataService.cs
@@ -0,0 +1,144 @@
+﻿using Devart.Data.PostgreSql;
+using DevExpress.DocumentServices.ServiceModel.DataContracts;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary.MetadataService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MetadataService" in both code and config file together.
+    public class MetadataService : IMetadataService
+    {
+        static IConfiguration configurationDB = new ConfigurationBuilder().AddJsonFile("AppSettings.json").Build();
+        readonly string connectionString = configurationDB["AppSettings:DatabaseConnection"];
+
+        public void AddParameter(int hoaId, int typeObject, string propertyName, int typeProperty, string systemName, string formula, bool staticParam, bool calculated)
+        {
+            using (PgSqlConnection conn = new PgSqlConnection(connectionString))
+            {
+                conn.Open();
+                PgSqlCommand pgSqlCommand = new PgSqlCommand("INSERT INTO metadata " +
+                    " (hoa_id, type_object, property_name, type_property, system_name, formula, \"static\", calculated)" +
+                    " VALUES (@hoa_id,@type_object,@property_name,@type_property,@system_name,@formula,@static,@calculated)", conn);
+                pgSqlCommand.Parameters.Add("@hoa_id", hoaId);
+                pgSqlCommand.Parameters.Add("@type_object", typeObject);
+                pgSqlCommand.Parameters.Add("@property_name", typeObject);
+                pgSqlCommand.Parameters.Add("@type_property", typeObject);
+                pgSqlCommand.Parameters.Add("@system_name", typeObject);
+                pgSqlCommand.Parameters.Add("@formula", typeObject);
+                pgSqlCommand.Parameters.Add("@static", typeObject);
+                pgSqlCommand.Parameters.Add("@calculated", typeObject);
+                pgSqlCommand.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+
+        public void AddTypeObject(string nameType, int parentType, int hoaId)
+        {
+            using (PgSqlConnection conn = new PgSqlConnection(connectionString))
+            {
+                conn.Open();
+                PgSqlCommand pgSqlCommand = new PgSqlCommand("Insert into types_objects (hoa_id,name,parent_type) values" +
+                    "(@hoa_id,@nameType,@parentType)", conn);
+                pgSqlCommand.Parameters.Add("@hoa_id", hoaId);
+                pgSqlCommand.Parameters.Add("@nameType", nameType);
+                pgSqlCommand.Parameters.Add("@parentType", parentType);
+                pgSqlCommand.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+
+        public void DeleteParameter(int parameterId)
+        {
+            using (PgSqlConnection conn = new PgSqlConnection(connectionString))
+            {
+                conn.Open();
+                PgSqlCommand pgSqlCommand = new PgSqlCommand("Delete from metadata where id = @id_value", conn);
+                pgSqlCommand.Parameters.Add("@id_value", parameterId);
+                pgSqlCommand.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+
+        public void DeleteTypeObject(int typeId)
+        {
+            using (PgSqlConnection conn = new PgSqlConnection(connectionString))
+            {
+                conn.Open();
+                PgSqlCommand pgSqlCommand = new PgSqlCommand("Delete from types_objects where id = @id_value", conn);
+                pgSqlCommand.Parameters.Add("@id_value", typeId);
+                pgSqlCommand.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+        public void EditParameter(int parameterId, string propertyName, int typeProperty, string systemName, string formula, bool staticParam, bool calculated)
+        {
+            using (PgSqlConnection conn = new PgSqlConnection(connectionString))
+            {
+                conn.Open();
+                PgSqlCommand pgSqlCommand = new PgSqlCommand("UPDATE metadata SET property_name = @propertyName, " +
+                    "type_property = @typeProperty, system_name = @systemName, formula = @formula, static = @staticParam," +
+                    "calculated = @calculated where id = @id", conn);
+                pgSqlCommand.Parameters.Add("@id", parameterId);
+                pgSqlCommand.Parameters.Add("@propertyName", propertyName);
+                pgSqlCommand.Parameters.Add("@typeProperty", typeProperty);
+                pgSqlCommand.Parameters.Add("@systemName", systemName);
+                pgSqlCommand.Parameters.Add("@formula", formula);
+                pgSqlCommand.Parameters.Add("@staticParam", staticParam);
+                pgSqlCommand.Parameters.Add("@calculated", calculated);
+                pgSqlCommand.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+
+        public void EditTypeObject(int typeId, string nameType, int parentType)
+        {
+            using (PgSqlConnection conn = new PgSqlConnection(connectionString))
+            {
+                conn.Open();
+                PgSqlCommand pgSqlCommand = new PgSqlCommand("UPDATE types_objects SET name = @nameType, " +
+                    "parent_type = @parentType where id = @id", conn);
+                pgSqlCommand.Parameters.Add("@id", typeId);
+                pgSqlCommand.Parameters.Add("@nameType", nameType);
+                pgSqlCommand.Parameters.Add("@parentType", parentType);
+                pgSqlCommand.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+
+        public DataSet GetParameters(int typeObject)
+        {
+            DataSet dataSet = new DataSet();
+            using (PgSqlConnection conn = new PgSqlConnection(connectionString))
+            {
+                conn.Open();
+                PgSqlCommand pgSqlCommand = new PgSqlCommand("select * from metadata where type_object = @typeObject", conn);
+                pgSqlCommand.Parameters.Add("@typeObject", typeObject);
+                PgSqlDataAdapter pgSqlDataAdapter = new PgSqlDataAdapter(pgSqlCommand);
+                pgSqlDataAdapter.Fill(dataSet);
+                conn.Close();
+            }
+            return dataSet;
+        }
+
+        public DataSet GetTypesObjects(int hoaId)
+        {
+            DataSet dataSet = new DataSet();
+            using (PgSqlConnection conn = new PgSqlConnection(connectionString))
+            {
+                conn.Open();
+                PgSqlCommand pgSqlCommand = new PgSqlCommand("select * from types_objects where hoa_id = @hoaId", conn);
+                pgSqlCommand.Parameters.Add("@hoaId", hoaId);
+                PgSqlDataAdapter pgSqlDataAdapter = new PgSqlDataAdapter(pgSqlCommand);
+                pgSqlDataAdapter.Fill(dataSet);
+                conn.Close();
+            }
+            return dataSet;
+        }
+    }
+}
