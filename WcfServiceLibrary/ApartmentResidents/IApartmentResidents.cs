31cd6fe2269d2ca4f6885f3543e80d94164a1e23
Богдан Т <bogdantribusyan2016@gmail.com>
2024-03-29 17:14:43 +0300

    Добавление сервиса "жильцы"

---
+++ WcfServiceLibrary/ApartmentResidents/IApartmentResidents.cs
@@ -0,0 +1,28 @@
+﻿using Devart.Data.PostgreSql;
+using System;
+using System.Data;
+using System.Runtime.Remoting.Proxies;
+using System.ServiceModel;
+
+namespace WcfServiceLibrary.ApartmentOwnersService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IApartmentOwnersService" in both code and config file together.
+    [ServiceContract]
+    public interface IApartmentResidents
+    {
+        [OperationContract]
+        void AddResident(string fullName,int objectId,bool registered, DateTime registration_date,
+            bool residence, DateTime checkInDate, bool owner, int numenator, int denominator);
+
+        [OperationContract]
+        void EditResident(int Id, string fullName, int objectId, bool registered, DateTime registration_date,
+            bool residence, DateTime checkInDate, bool owner, int numenator, int denominator);
+
+        [OperationContract]
+        void DeleteResident(int Id);
+
+        [OperationContract]
+        DataSet GetResidents(int objectId);
+
+    }
+}
