bac4aeb26c7b65f99303649cba4915c9b8a079e3
Богдан Т <bogdantribusyan2016@gmail.com>
2024-05-28 21:53:26 +0300

    Изменение метода получения скелета динамических параметров

--- WcfServiceLibrary/DPService/DynamicParamsService.cs
+++ WcfServiceLibrary/DPService/DynamicParamsService.cs
@@ -61,27 +61,31 @@ namespace WcfServiceLibrary.DPService
             using (PgSqlConnection conn = new PgSqlConnection(connectionString))
             {
                 conn.Open();
-                PgSqlCommand pgSqlCommand = new PgSqlCommand("WITH RECURSIVE search_tree(id,type_object,identificator, parent_id) AS ( " +
+                PgSqlCommand pgSqlCommand = new PgSqlCommand("WITH RECURSIVE search_tree(id,type_object,identificator, parent_id) AS (  " +
                     "SELECT objects.id,objects.type_object,objects.identificator, objects.parent_id " +
                     "FROM objects " +
                     "where objects.id = :object_id " +
                     "UNION ALL " +
-                    "SELECT t.id, t.type_object, t.identificator,t.parent_id " +
-                    "FROM objects t, search_tree st " +
-                    "WHERE st.id = t.parent_id ) " +
-                    "SELECT search_tree.id,search_tree.type_object,parent_id,name || ' ' || identificator as Name,  " +
-                    "account,metadata.property_name,metadata.id,period,value,personal_account_id " +
+                    "SELECT t.id, t.type_object, t.identificator,t.parent_id" +
+                    " FROM objects t, search_tree st " +
+                    "WHERE st.id = t.parent_id )  " +
+                    "SELECT search_tree.id as object_id,search_tree.type_object,parent_id,types_objects.name || ' ' || identificator as Name, " +
+                    "account,metadata.property_name,metadata.id as metadata_id ,dynamicTable.period, dynamicTable.value,personal_account_id , " +
+                    "types_tariffs.name as tariff_name,types_tariffs.id as tariff_id " +
                     "FROM search_tree " +
                     "join types_objects on types_objects.id = search_tree.type_object " +
                     "left join personal_account on personal_account.object_id = search_tree.id " +
                     "left join metadata on metadata.type_object = search_tree.type_object " +
-                    "left join (select distinct on (property_id,personal_account_id) property_id,period, value,object_id, property_name, personal_account_id " +
-                        "from dynamic_params " +
-                        "join metadata on metadata.id = property_id " +
-                        "where type_object = :type_object " +
-                        "order by property_id,personal_account_id,period desc) as dynamicTable " +
-                    "on dynamicTable.personal_account_id = personal_account.id  and metadata.id = property_id " +
-                    "where search_tree.type_object = :type_object and static = 'false' and calculated = 'false'", conn);
+                    "join types_tariffs on types_tariffs.metadata_id = metadata.id " +
+                    "left join (" +
+                    "select distinct on (property_id,personal_account_id) property_id,period, dynamic_params.value, " +
+                    "object_id, property_name, personal_account_id, tariff_id " +
+                    "from dynamic_params" +
+                    " join metadata on metadata.id = property_id " +
+                    "where type_object = :type_object " +
+                    "order by property_id,personal_account_id,period desc) as dynamicTable " +
+                    "on dynamicTable.personal_account_id = personal_account.id  and metadata.id = property_id and dynamicTable.tariff_id = types_tariffs.id" +
+                    " where search_tree.type_object = :type_object and static = 'false' and calculated = 'false'", conn);
                 pgSqlCommand.Parameters.Add(":object_id", objectId);
                 pgSqlCommand.Parameters.Add(":type_object", typeObject);
                 using (PgSqlDataAdapter pgSqlDataAdapter = new PgSqlDataAdapter(pgSqlCommand))
