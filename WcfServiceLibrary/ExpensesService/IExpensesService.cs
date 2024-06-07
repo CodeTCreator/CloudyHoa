db46d4c5ab80e7e978e2d19b54ded71792de42a4
Богдан Т <bogdantribusyan2016@gmail.com>
2024-06-07 11:22:08 +0300

    Добавление сервиса раходов организаций

---
+++ WcfServiceLibrary/ExpensesService/IExpensesService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary.ExpensesService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IExpensesService" in both code and config file together.
+    [ServiceContract]
+    public interface IExpensesService
+    {
+        [OperationContract]
+        void AddCategory(string name);
+
+        [OperationContract]
+        void EditCategory(int categoryId, string name);
+
+        [OperationContract]
+        void DeleteCategory(int categoryId);
+
+
+        [OperationContract]
+        void AddExpense(int categoryId, string name, float quantity, float cost, float resultCost);
+
+        [OperationContract]
+        void EditExpense(int expenseId, int categoryId, string name, float quantity, float cost, float resultCost);
+
+        [OperationContract]
+        void DeleteExpense(int expenseId);
+
+        [OperationContract]
+        DataSet GetCategories();
+
+        [OperationContract]
+        DataSet GetExpenses(DateTime dateTime);
+
+        [OperationContract]
+        DataSet GetAllExpenses();
+    }
+}
