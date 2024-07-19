5503b39a17e0593b63c41c1d0c811048f2bd85e4
Богдан Т <bogdantribusyan2016@gmail.com>
2024-07-19 14:50:42 +0300

    Сервис для расчетов

---
+++ WcfServiceLibrary/CalculationService/ICalculationsService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICalculationsService" in both code and config file together.
+    [ServiceContract]
+    public interface ICalculationsService
+    {
+        [OperationContract]
+        DataSet GetCalculations(int objectId, DateTime period);
+
+        [OperationContract]
+        DataSet GetTemplate(int propId, DateTime period);
+
+        [OperationContract]
+        void AddCalculation(int propId, DateTime period, float value, int paId);
+
+        [OperationContract]
+        void EditCalculation(int propId, DateTime period, float value, int paId, int id);
+
+        [OperationContract]
+        void DeleteCalcuation(int id);
+    }
+}
